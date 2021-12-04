8f6fa32b0e03b75e4d2e6d15b23beaaf8b22b7e2
jessieliu <jliu997@gmail.com>
2021-12-03 22:01:35 -0500

    🎁day 3 complete🎁
    a lil gross. today I learned that the unary NOT operator is ~. also learned that there are no unsigned ints in python so couldn't use the operator

--- util/util.py
+++ util/util.py
@@ -9,3 +9,9 @@ def file_to_tuple_list(filename):
       input_lines = [tuple(line.strip().split()) for line in file]
       file.close()
       return input_lines
+
+def file_to_string_list(filename):
+    with open(filename, 'r') as file:
+      input_lines = [line.strip() for line in file]
+      file.close()
+      return input_lines
